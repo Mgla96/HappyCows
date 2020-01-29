60f31218a4078e3b3c81baef13ce8aa5371b4caf
Matt Gottlieb <mattgottlieb96@gmail.com>
2020-01-29 14:07:53 -0800

    I think only entities are needed and not relationships for sequelize's source target models

--- routes/commons.js
+++ routes/commons.js
@@ -1,12 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Commons = sequelize.define('commons', {
-    comID: DataTypes.INTEGER,
+  var Commons = sequelize.define('Commons', {
+    comID: DataTypes.UUID,
     primaryKey: true,
-    autoIncrement: true
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false
+    //autoIncrement: true
   });
 
   Commons.associate = function(models){ //figure out relations
+    models.Commons.hasOne(models.Config)
+    models.Commons.hasMany(models.Users)
+    
     //models.Commons.hasMany(models.playing);
     //models.Commons.hasMany(models.contains);
   };
--- routes/config.js
+++ routes/config.js
@@ -2,13 +2,19 @@
 module.exports = (sequelize, DataTypes) => {
   var Config = sequelize.define('cows', {
     conID: {
-    type: DataTypes.INTEGER,
+    type: DataTypes.UUID,
     primaryKey: true,
-    autoIncrement: true},
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false
+    //autoIncrement: true
+    },
     milkTime: DataTypes.STRING,
     cowPrice: DataTypes.STRING,
     tax: DataTypes.STRING,
-    startDate: DataTypes.DATE,
+    createdAt:{ 
+      type: DataTypes.DATE,
+      allowNull: false
+    },
     endDate: DataTypes.DATE
   });
 
