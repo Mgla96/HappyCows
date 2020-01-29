405c180a946955d6c6aaa67379aea030a35fc678
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-01-28 22:04:46 -0800

    working on sql tables in routes

--- routes/commons.js
+++ routes/commons.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Commons = sequelize.define('Commons', {
-    comID: DataTypes.STRING
+    comID: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
   });
 
   Commons.associate = function(models){
---
+++ routes/in.js
@@ -0,0 +1,13 @@
+//relationship between Commons and Cows'use strict';
+module.exports = (sequelize, DataTypes) => {
+    var In = sequelize.define('In', {
+      
+    });
+  
+    In.associate = function(models){
+     
+    };
+  
+    return In;
+  
+  }
