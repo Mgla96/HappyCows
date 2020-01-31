92b58ab41a3b6b4df3e116f9ea419946dabb04bb
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-01-31 13:28:27 -0800

    adding migrations. migrations ahve the incremented ids and timestamps but seeing if I need to add them back to models too.

--- models/commons.js
+++ models/commons.js
@@ -1,21 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Commons = sequelize.define('Commons', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    name: Sequelize.STRING,
-    admin_uid: Sequelize.INTEGER
-  });
-
-  Commons.associate = function(models){ //figure out relations
+  const Commons = sequelize.define('Commons', {
+    name: DataTypes.STRING,
+    admin_uid: DataTypes.INTEGER
+  }, {});
+  Commons.associate = function(models) {
     models.Commons.hasOne(models.Config)
     models.Commons.hasMany(models.Users)
     models.Commons.hasMany(models.Cows)
   };
-
   return Commons;
-
-}
+};
