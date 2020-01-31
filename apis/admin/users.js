58058a9b7b162a90bd87aaa6f345af1cd5cddd73
Shou C <scf@ieee.org>
2020-01-30 16:10:54 -0800

    Update users.js

--- apis/admin/users.js
+++ apis/admin/users.js
@@ -2,4 +2,9 @@ function get_user(req, res) {
   res.json({})
 }
 
-export default { get_user };
+function get_users_with_id(req, res) {
+  let user_id = req.params.id
+  res.json({})
+}
+
+export default { get_user, get_users_with_id };
