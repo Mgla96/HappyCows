e94b3af3e2f0741f7d6598bd24e8e22eac100dc3
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-02-13 16:50:22 -0800

    some more database functions

--- apis/users/index.js
+++ apis/users/index.js
@@ -1,17 +1,17 @@
 var db = require("../../models/index");
 db.Cows.sync();
 
-function get_user_wealth() {
+function get_user_wealth(req, res) {
     //calls function from user_in_commons
 }
 
-function get_user_day_profit(){
+function get_user_day_profit(req, res){
 
 }
-function buy_cow(){
+function buy_cow(req, res){
 
 }
 
-function sell_cow(){
+function sell_cow(req, res){
 
 }
