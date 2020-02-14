0653e16a399506c0a0eaadaa4e6e62f02522e88f
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-02-13 16:50:22 -0800

    some more database functions

--- apis/admin/common_config.js
+++ apis/admin/common_config.js
@@ -1,10 +1,52 @@
 
-function cow_price(){
+function cow_price(req, res){
+    const commonId = req.params.id;
+    db.Config.findAll({
+		attributes: ['cowPrice'], 
+		where: {id: commonId}
+	}).then((dbRes)=>{
+		if (dbRes.length == 0) {
+			res.json({success: false, message: "There's no common found to change config"})
+		}
+		else {
+			res.json({success: true, data: dbRes})
+		}
+	})
+}
+
+function milk_time(req, res){
+    const commonId = req.params.id;
+    db.Config.findAll({
+		attributes: ['milkTime'], 
+		where: {id: commonId}
+	}).then((dbRes)=>{
+		if (dbRes.length == 0) {
+			res.json({success: false, message: "There's no common found to change config"})
+		}
+		else {
+			res.json({success: true, data: dbRes})
+		}
+	})
 
 }
 
-function global_health(){
-
+/*
+* Working on global_health 
+*/
+function global_health(req, res){
+    const commonId = req.params.id;
+	db.Cows.findAll({
+        attributes: [commonId, [models.sequelize.fn('AVG', models.sequelize.col('health')),'healthAvg']], 
+        group: [commonId],
+        order: [[models.sequelize.fn('AVG', models.sequelize.col('health')), 'DESC']]
+    }).then((dbRes)=>{
+		if (dbRes.length == 0) {
+			res.json({success: false, message: "Could not get global health"})
+		}
+		else {
+			res.json({success: true, data: dbRes})
+		}
+	})
 }
 
 function get_create_date(){
--- apis/cows/index.js
+++ apis/cows/index.js
@@ -1,14 +1,21 @@
 var db = require("../../models/index");
 db.Cows.sync();
 
-function get_cow_health() {
-
+/**
+ * Don't need because get_cow in admin cows returns health
+ * 
+ **/
+function get_cow_health(req, res ) {
+    const cowId = req.params.id;
+    db.Cows.findAll({
+        attributes: ['health'],
+        where: {id: cowId}
+    }).then((dbRes)=>{
+        if (dbRes.length == 1) {
+            res.json({success: true, data: dbRes[0]})
+        }
+        else {
+            res.json({success: false, message: "No cow found"})
+        }
+    })
 }
-
-
-
-
-
-
-
-
