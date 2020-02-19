8f8b36078e98c789566ab407ca4744af5e7ebeb7
Shou C <scf@ieee.org>
2020-02-19 09:31:09 -0800

    MVVW => MVC

--- apis/admin/user_in_common.js
+++ apis/admin/user_in_common.js
@@ -1,14 +1,13 @@
-function get_user_wealth(req, res){
-    const userID = req.params.id;
-	db.User_Wealth.findAll({
+function get_user_wealth(userId){
+	db.UserWealth.findAll({
 		attributes: ['wealth'],
 		where: {id: userID}
 	}).then((dbRes)=>{
 		if (dbRes.length == 1) {
-			res.json({success: true, data: dbRes[0]})
+			return true, dbRes[0]
 		}
 		else {
-			res.json({success: false, message: "No user found"})
+			return false, null
 		}
 	})
 }
