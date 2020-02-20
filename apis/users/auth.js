bc6a2015c09bdc55f90d921a97bc58a9fb50100b
Shou C <scf@ieee.org>
2020-02-20 09:30:27 -0800

    fix user

--- apis/users/auth.js
+++ apis/users/auth.js
@@ -1,26 +1,15 @@
 const {get_auth_url,get_email_from_at, get_token_for_email} = require('../../utils/auth');
 
-async function get_link(req, res){
-    res.json({
-        success: true,
-        data: get_auth_url()
-    })
+async function get_link(){
+    return true, get_auth_url;
 }
 
-async function auth(req, res){
-    const {google_token} = req.body;
+async function auth(google_token){
     const email = await get_email_from_at(google_token)
     if (!email){
-        res.json({
-            data: "",
-            success: false,
-            message: "Google's fault"
-        })
+        return false, null
     }
-    res.json({
-        data: await get_token_for_email(email),
-        success: true
-    })
+    return true, await get_token_for_email(email)
 }
 
 module.exports = { 
