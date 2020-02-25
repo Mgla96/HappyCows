6ea318b69f8a1002ea778cb308e7563ef2148015
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-02-25 15:10:32 -0800

    small fix to data tables and documentation

--- apis/admin/user_in_common.js
+++ apis/admin/user_in_common.js
@@ -1,7 +1,12 @@
-function get_user_wealth(userId){
+/*
+User can have different wealth in different commons
+
+*/
+
+function get_user_wealth(userId, cId){
 	db.UserWealth.findAll({
 		attributes: ['wealth'],
-		where: {id: userID}
+		where: {id: userID, commonsId: cId}
 	}).then((dbRes)=>{
 		if (dbRes.length == 1) {
 			return true, dbRes[0]
