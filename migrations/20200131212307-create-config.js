846c7ac284c51cc4a7a31cb95bb110649cf34a7f
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-03-07 15:43:52 -0800

    fixing table names

--- migrations/20200131212307-create-config.js
+++ migrations/20200131212307-create-config.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Configs', {
+    return queryInterface.createTable('Config', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Configs');
+    return queryInterface.dropTable('Config');
   }
 };
