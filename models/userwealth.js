09536a5bac2cdc1465ca6d68d6c6673e7e361534
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-03-07 12:32:38 -0800

    changing structure of user wealth table

--- models/userwealth.js
+++ models/userwealth.js
@@ -4,8 +4,8 @@ module.exports = (sequelize, DataTypes) => {
     wealth: DataTypes.INTEGER
   }, {});
   UserWealth.associate = function(models) {
-    models.UserWealth.belongsTo(models.Users); // association key in UserWealth model
-    models.UserWealth.belongsTo(models.Commons); //association key in UserWealth model 
+    models.UserWealth.belongsTo(models.Users) // association key in UserWealth model
+    models.UserWealth.belongsTo(models.Commons) //association key in UserWealth model 
     // associations can be defined here
   };
   return UserWealth;
