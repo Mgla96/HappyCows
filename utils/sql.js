3844a2dcccbb56698898d7d3799689d1f231f1e5
Shou C <scf@ieee.org>
2020-03-07 16:57:44 -0800

    finish linking

--- utils/sql.js
+++ utils/sql.js
@@ -1,4 +1,6 @@
 module.exports = {
     get_wealth: "SELECT SUM(wealth) FROM UserWealths WHERE UserId = ",
-    get_cows: "SELECT COUNT(wealth) FROM Cows WHERE UserId = "
+    get_cows: "SELECT COUNT(wealth) FROM Cows WHERE UserId = ",
+    get_health: "SELECT AVG(health) FROM Cows WHERE CommonId = ",
+    get_players: "SELECT COUNT(*) FROM UserCommons WHERE CommonId = "
 };
--- views/admin_common_post.js
+++ views/admin_common_post.js
@@ -1,13 +1,14 @@
 const {create_common} = require("../apis/admin/commons");
 
 module.exports = async (req, res) => {
+    console.log(res.locals.user_id)
     await create_common(
         req.body.name,
-        req.body.user_id,
+        res.locals.user.id,
         req.body.cow_price,
         req.body.milk_time,
-        req.body.start_date,
-        req.body.end_date
+        new Date(req.body.start_date),
+        new Date(req.body.end_date)
     );
     res.redirect("/admin/");
 };
