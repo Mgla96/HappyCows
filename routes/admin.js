2cf21d990750af9ba37ffbfc9c750a8d45ab3909
Shou C <scf@ieee.org>
2020-03-07 16:09:08 -0800

    work on admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var admin_common_get = require('../views/admin_common_get');
+var admin_common_post = require('../views/admin_common_post');
 var admin_get = require('../views/admin_get');
-
-router.get('/common', admin_get);
+router.get('/', admin_get);
+router.get('/common', admin_common_get);
+router.post('/common', admin_common_post);
 
 module.exports = router;
