560b6b5565e3d23e170666416bf4883b10fe4a16
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-03-09 16:29:33 -0700

    choose commons skeleton

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 const main = require("../views/main")
+const commonschoice = require("../views/user_choose_commons")
 let {auth_middleware} = require("../utils/auth")
 
 /* GET home page. */
 router.get('/', auth_middleware, main);
-
+router.get('/choosecommons', auth_middleware, commonschoice);
 module.exports = router;
---
+++ views/user_choose_commons.js
@@ -0,0 +1,8 @@
+module.exports = (req, res)=>{
+    const user_obj = res.locals.user;
+    res.render('user_select_commons', {data : 
+        {
+            
+        }
+    }
+)}
