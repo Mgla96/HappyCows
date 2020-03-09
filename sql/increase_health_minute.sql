83ede0325bab9b92dbac5221dc2a0b16a3b8bf7b
Shou C <scf@ieee.org>
2020-03-09 13:52:04 -0700

    Create increase_health_minute.sql

---
+++ sql/increase_health_minute.sql
@@ -0,0 +1,16 @@
+CREATE EVENT IF NOT EXISTS `increase_health_minute`
+ON SCHEDULE
+  EVERY 1 MINUTE
+  COMMENT 'Increase health if lower than threshold'
+  DO
+    UPDATE Cows AS c SET
+            health = c.health + 0.001 - 0.001 *
+                (SELECT COUNT(temp.id) FROM (SELECT * FROM Cows) AS temp WHERE temp.CommonId = c.CommonId) /
+                ((SELECT maxCowPerPerson FROM Configs WHERE CommonId = c.CommonId) *
+                (SELECT COUNT(*) FROM UserCommons  WHERE CommonId = c.CommonId))
+        WHERE
+            (SELECT COUNT(temp.id) FROM (SELECT * FROM Cows) AS temp WHERE temp.CommonId = c.CommonId) /
+            ((SELECT maxCowPerPerson FROM Configs WHERE CommonId = c.CommonId) *
+            (SELECT COUNT(*) FROM UserCommons  WHERE CommonId = c.CommonId)) < 1
+            AND c.health < 100 AND c.health > 0
+
