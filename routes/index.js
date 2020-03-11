dc703414be941aace03ac2d7702051081f336e64
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-03-10 19:42:22 -0700

    trying to get joining commons to work

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const main = require("../views/main")
 const commonschoice = require("../views/user_choose_commons")
+//const commonsjoin = require("../views/user_common_join")
 let {auth_middleware} = require("../utils/auth")
 
 /* GET home page. */
