31c82ba58f2caed6f97e5d3f85e1f05a2447e672
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-03-24 18:29:31 -0700

    more routing

--- views/user_sell_cow_post.js
+++ views/user_sell_cow_post.js
@@ -1,9 +1,9 @@
 const {user_sell_cow} = require("../apis/users/index");
 
 module.exports = async (req, res) => {
+    //console.log(res.locals.user_id);
     await user_sell_cow(
         req.body.cid,
         res.locals.user.id,
     );
-    res.redirect("/play");
 };
