42a19749a60951a78bd64bfabcda4dc489c12e12
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-03-27 09:17:31 -0700

    working admin taxes

--- views/admin_common_get.js
+++ views/admin_common_get.js
@@ -1,5 +1,5 @@
 const {get_users_in_common} = require("../apis/admin/user_in_common");
-const {get_cow_price,get_max_cow,get_degrade_rate,get_start_date,get_end_date,get_milk_price} = require("../apis/admin/common_config");
+const {get_cow_price,get_max_cow,get_degrade_rate,get_start_date,get_end_date,get_milk_price,get_tax_rate} = require("../apis/admin/common_config");
 
 module.exports = async (req, res) => {
     const users = await get_users_in_common(req, req.params.commonId);
@@ -9,6 +9,7 @@ module.exports = async (req, res) => {
     const startDate = await get_start_date(res.locals.user.id);
     const endDate = await get_end_date(res.locals.user.id);
     const milkPrice = await get_milk_price(res.locals.user.id);
+    const taxPrice = await get_tax_rate(req.params.commonId);
 
     res.render('admin_commons',
         {data :
@@ -20,7 +21,8 @@ module.exports = async (req, res) => {
                 degradeRate: degradeRate,
                 startDate: startDate,
                 endDate: endDate,
-                milkPrice: milkPrice
+                milkPrice: milkPrice,
+                taxPrice: taxPrice
             }
         }
     )
