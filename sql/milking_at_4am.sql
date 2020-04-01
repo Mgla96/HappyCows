6e1880528d97a8d0041dd747f3a82fe763b2da3e
Shou C <scf@ieee.org>
2020-04-01 16:05:38 -0700

    fix milking

--- sql/milking_at_4am.sql
+++ sql/milking_at_4am.sql
@@ -3,13 +3,14 @@ ON SCHEDULE
   EVERY 4 DAY_HOUR
   COMMENT 'Milk the cows at 4AM everyday'
   DO
-    INSERT INTO UserWealths (wealth, "milk", createdAt, updatedAt, CommonId, UserId)
+    INSERT INTO UserWealths (wealth, type, createdAt, updatedAt, CommonId, UserId)
         SELECT
-            (SELECT milkPrice FROM Configs AS conf WHERE conf.CommonId = CommonId) *
+            (SELECT milkPrice FROM Configs AS conf WHERE conf.CommonId = cw.CommonId) *
             (SELECT tax FROM TieredTaxings AS t INNER JOIN
-                Configs AS c ON c.Id = t.ConfigId WHERE c.CommonId = CommonId)
+                Configs AS c ON c.Id = t.ConfigId WHERE c.CommonId = cw.CommonId) / 100
             AS wealth,
+            "milk" AS `status`,
             createdAt,
             updatedAt,
             CommonId,
-            UserId FROM Cows
+            UserId FROM Cows AS cw
