2084fc82db6f5dc1f11ab24090a07cca32a563fa
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-04-13 14:49:29 -0700

    bug fixes - sell cow not routing right and milk price always initializing to 0

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ console.log(get_users({
 var app = express();
 
 //Uncomment this line to place tables back in database
-/*
+
 db.Users.sync();
 db.UserCommons.sync();
 db.Commons.sync();
@@ -30,7 +30,7 @@ db.Configs.sync();
 db.Cows.sync();
 db.TieredTaxings.sync();
 db.UserWealths.sync();
-*/
+
 //testing api section
 // view engine setup
 app.set('views', path.join(__dirname, 'templates'));
