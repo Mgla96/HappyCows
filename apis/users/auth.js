c29c7e26d0ff99366fd79c1a020a3e181a5b2070
mgla96 <mattgottlieb96@gmail.com>
2020-10-20 12:00:49 -0400

    stuff

---
+++ apis/users/auth.js
@@ -0,0 +1,26 @@
+const {get_auth_url,get_email_from_at, get_token_for_email} = require('../../utils/auth');
+
+async function get_link(){
+    return true, get_auth_url();
+}
+
+async function auth(google_token){
+    const {email, first_name, last_name} = await get_email_from_at(google_token)
+    if (!email){
+        return {
+            success: false, 
+            data: null
+        }
+    }
+    let data = await get_token_for_email(email, first_name, last_name)
+    return {
+        success: true, 
+        data: data
+    }
+}
+
+module.exports = { 
+    get_link, 
+    auth
+};
+  
